e0b40999fa1b4145be5ecc55ea876ba641974f9a
DSVAngel <143917687+DSVAngel@users.noreply.github.com>
2023-10-05 19:19:27 +0000

    "Cambios en JSON"

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
+  name: "Angel Solano",
   title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  email: "sangel.contact@gmail.com",
+  gitHub: "DSVAngel",
+  instagram: "solano_2505",
+  linkedIn: "",
   medium: "",
-  twitter: "microsoft",
+  twitter: "",
   youTube: "Code",
 };
 
